a5d52d2eec4c39ebdc77010ee4af2a1234744fd8
jadersonfarias <jader375@gmail.com>
2025-07-10 19:56:48 -0300

    upadate ia

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,20 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomQuestions } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { uploadAudioRoute } from './http/routes/upload-audio.ts';
+import fastifyMultipart from '@fastify/multipart';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 });
+
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -21,6 +28,10 @@ app.get('/health', () => {
   return 'ok';
 });
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestions);
+app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
-app.listen({ port: env.PORT })
+app.listen({ port: env.PORT });
